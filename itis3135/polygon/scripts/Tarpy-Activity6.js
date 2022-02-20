3cbf9be996c8526ac3d658416ed40a7900e5ec91
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-02-20 17:34:24 -0500

    Update Tarpy-Activity6.js

--- itis3135/polygon/scripts/Tarpy-Activity6.js
+++ itis3135/polygon/scripts/Tarpy-Activity6.js
@@ -1,48 +1,51 @@
 var sides;
 
 function getShape(sides)
-var text;
-var sideShape = prompt("How many sides would you like to enter (3-8)");
-switch(sides)
 {
-  case 3:
-    alert(text = "triangle");
-    break;
-  case 4:
-    alert(text = "polygon");
-    break;
-  case 5:
-    alert(text = "pentagon");
-    break;
-  case 6:
-    alert(text = "hexagon");
-    break;
-  case 7:
-    alert(text = "heptagon");
-    break;
-  case 8:
-    alert(text = "octogon");
-    break;
-  default:
-    alert(text = "Enter a number 3-8");
-    function(sides);
-    break;
+
+  var text;
+  var sideShape = prompt("How many sides would you like to enter (3-8)");
+  switch(sides)
+  {
+    case 3:
+      alert(text = "triangle");
+      break;
+    case 4:
+      alert(text = "polygon");
+      break;
+    case 5:
+      alert(text = "pentagon");
+      break;
+    case 6:
+      alert(text = "hexagon");
+      break;
+    case 7:
+      alert(text = "heptagon");
+      break;
+    case 8:
+      alert(text = "octogon");
+      break;
+    default:
+      alert(text = "Enter a number 3-8");
+      break;
+  }
 }
 
-function validateEntry(sides)
-{
-  if(isNaN(sides))
+  function validateEntry(sides)
   {
-    sides = alert("Enter a valid number");
-  }
-  
-  if (sides % 1 != 0)
-  {
-    sides = Math.round(sides)
-    return sides;
-  }
-  else if (sides < 0)
-  {
-    sides = Math.abs(sides)
-    return sides;
+    if(isNaN(sides))
+    {
+      sides = alert("Enter a valid number");
+    }
+
+    if (sides % 1 != 0)
+    {
+      sides = Math.round(sides)
+      return sides;
+    }
+    else if (sides < 0)
+    {
+      sides = Math.abs(sides)
+      return sides;
+    }
   }
