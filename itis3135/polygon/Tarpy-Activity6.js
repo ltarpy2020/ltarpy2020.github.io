9fd4f57e5334978c7d311f0d2959b59c7cf78da2
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-02-19 22:23:04 -0500

    Update Tarpy-Activity6.js

--- itis3135/polygon/Tarpy-Activity6.js
+++ itis3135/polygon/Tarpy-Activity6.js
@@ -1,8 +1,28 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 js}
+function getShape(sideShape)
+var text;
+var sideShape = prompt("How many sides would you like to enter (3-8)");
+switch(sideShape)
+{
+  case 3:
+    alert(text = "triangle");
+    break;
+  case 4:
+    alert(text = "polygon");
+    break;
+  case 5:
+    alert(text = "pentagon");
+    break;
+  case 6:
+    alert(text = "hexagon");
+    break;
+  case 7:
+    alert(text = "heptagon");
+    break;
+  case 8:
+    alert(text = "octogon");
+    break;
+  default:
+    alert(text = "Enter a number 3-8");
+    function(sideShape);
+    break;
+}
