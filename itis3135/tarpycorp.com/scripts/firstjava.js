971b3e0ad10ddb3a53e4aebc0504648c98cf1e19
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-04-30 18:08:38 -0400

    Update firstjava.js

--- itis3135/tarpycorp.com/scripts/firstjava.js
+++ itis3135/tarpycorp.com/scripts/firstjava.js
@@ -3,15 +3,15 @@ function headsOrTails()
 {
   let headsOrTails = 0;
   let computer = 0;
-  let wins = 0;
   computer = Math.floor(Math.random() * 2);
   headsOrTails = Math.floor(Math.random() * 2);
   if (computer === headsOrTails)
   {
-    wins = "win";
+    document.getElementById('result').innerHTML = 'win';
   }
   else if (computer != headsOrTails)
   {
-    wins = "lose"
+    document.getElementById('result').innerHTML = 'lose';
   }
 }
+document.getElementById('go').addEventListener('click', headsOrTails);
