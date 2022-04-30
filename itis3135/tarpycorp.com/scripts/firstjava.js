d56612cc0117e7896508d60e93f01a6eddff19fc
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-04-30 18:27:12 -0400

    Update firstjava.js

--- itis3135/tarpycorp.com/scripts/firstjava.js
+++ itis3135/tarpycorp.com/scripts/firstjava.js
@@ -14,3 +14,42 @@ function headsOrTails()
   }
 }
 document.getElementById('go').addEventListener('click', headsOrTails);
+
+function rps()
+{
+  let user = 0;
+  let compute = 0;
+  compute = Math.floor(Math.random() * 3);
+  user = Math.floor(Math.random() * 3);
+  if (user === compute)
+  {
+    document.getElementById('rocks').innerHTML = 'Its a tie';
+  }
+  else if (user != compute)
+  {
+    if (user === 0 && compute === 1)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses rock and wins!';
+    }
+    if (user === 0 && compute === 2)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses rock and loses';
+    }
+    if (user === 1 && compute === 0)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses scissor and loses';
+    }
+    if (user === 1 && compute === 2)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses scissor and wins!';
+    }
+    if (user === 2 && compute === 0)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses paper and wins!';
+    }
+    if (user === 2 && compute === 1)
+    {
+      document.getElementById('rocks').innerHTML = 'User chooses paper and loses';
+    }
+  }
+  document.getElementById('go').addEventListener('click', rps);
