e50c01670a87dd6b9917eb2bb58047989c6cacbe
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-01 15:03:16 -0400

    Update jarray.js

--- itis3135/tarpycorp.com/scripts/jarray.js
+++ itis3135/tarpycorp.com/scripts/jarray.js
@@ -28,17 +28,8 @@ a.addEventListener('click', function()
   }
 });
 
-function displayed()
+d.addEventListener('click', function()
 {
-  for (let i = 0; i < people.length; i++)
-  {
-    let row = t.insertRow(i);
-    let r1 = row.insertCell(0);
-    let r2 = row.insertCell(1);
-    r1.innerHTML = people[i];
-    r2.innerHTML = x[i];
-    if (i > 0) 
-    {
-      t.deleteRow(i - 1);
-    }
-  }
+  document.getElementById('re').innerHTML = people;
+  document.getElementById('result').innerHTML = x;
+});
