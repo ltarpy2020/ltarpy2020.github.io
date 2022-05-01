2d845f4b04c6a41b2595eadf3a871d7fdf55dab8
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-01 17:43:37 -0400

    Update slidejs.js

--- itis3135/tarpycorp.com/scripts/slidejs.js
+++ itis3135/tarpycorp.com/scripts/slidejs.js
@@ -1,8 +1,28 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+let sli = 1;
+showSlides(sli);
 
-\f0\fs24 \cf0 js}
+function currentSlide(x)
+{
+  showSlides(sli = x)
+}
+
+function showSlides(x)
+{
+  let i = 0;
+  let slides = document.getElementsByClassName('mySlides');
+  let d = document.getElementsByClassName('dot');
+  if (n > slides.length)
+  {
+    sli = 1;
+  }
+  if (n < 1)
+  {
+    sli = sides.length
+  }
+  for (i = 0; i < d.length; i++)
+  {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }  
+  slides[sli-1].style.display = "block";
+  d[sli-1].className += " active";
+}
