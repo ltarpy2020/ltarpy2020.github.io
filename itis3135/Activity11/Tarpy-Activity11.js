b9f2fceafee20fb3e8abc45c49407fb4a83a7a4b
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-02 19:51:32 -0400

    Update Tarpy-Activity11.js

--- itis3135/Activity11/Tarpy-Activity11.js
+++ itis3135/Activity11/Tarpy-Activity11.js
@@ -1,4 +1,10 @@
 <!-- jQuery call to the accordion() method. -->
 $(document).ready(function() {
-    $("#tabs").tabs();
+    $("#accordian").accordian(
+        {
+            collapsible: true,
+            active: false,
+            heightstyle: content;
+        }
+    );
 });
