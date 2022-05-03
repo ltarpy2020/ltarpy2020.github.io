5b0e3b6c7be5484490b8fa66d29a41103e2643ce
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-03 19:46:10 -0400

    Update Activity14.js

--- itis3135/Activity14/Activity14.js
+++ itis3135/Activity14/Activity14.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
+	let t = document.getElementById('toobin');
+	let o = document.getElementById('sorkin');
+	let c = document.getElementById('chua');
+	let s = document.getElementById('sampson');
+	
+	t.addEventListener('click', function()
+	{
+		$.getJSON('json_files/toobin.json');
+	});
+	o.addEventListener('click', function()
+	{
+		$.getJSON('json_files/sorkin.json');
+	});
+	c.addEventListener('click', function()
+	{
+		$.getJSON('json_files/chua.json');
+	});
+	s.addEventListener('click', function()
+	{
+		$.getJSON('json_files/sampson.json');
+	});
+	
 	
 }); // end ready
