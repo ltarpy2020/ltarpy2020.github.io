425cc477c17ee0cae5cb65fa7fef360a7fb0b3b1
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-04 15:26:50 -0400

    Update Activity15.js

--- itis3135/Activity15/Activity15.js
+++ itis3135/Activity15/Activity15.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-
+$(document).ready(function()
+{
+   $.getJSON('facultyList.json')
+   
+  let fImage = $("<img>");
+  let fName = $("<h2>");
+  let fSection = $("<h3>");
+  let fBio = $("<p>");
+  
+  this.function(data)
+  {
+    $.each(data, function()
+    {
+      $.each(this, function()
+      {
+        $("#faculty").append(fImage);
+        $("#faculty").append(fName);
+        $("#faculty").append(fSection);
+        $("#faculty").append(fBio);
+      })
+    })
+  }
 });
