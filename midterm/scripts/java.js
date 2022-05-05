30d190e2e327681bf9da67961527e9ff8e084ca8
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-05 16:54:46 -0400

    Update java.js

--- midterm/scripts/java.js
+++ midterm/scripts/java.js
@@ -23,13 +23,12 @@ function funFact()
 }
 document.getElementById('fact').addEventListener('click', funFact);
 
-$(document).ready(function() {
-    $("#slides").bxSlider({
-        auto: true,
-        minSlides: 1,
-        maxSlides: 1,
-        slideWidth: 500,
-        slideMargin: 20,
-        speed: 3000,
-    });
-});
+function switchPics()
+{
+  let s = document.getElementById('im');
+  let c = document.getElementById('cmc');
+  s.addEventListener('click', function()
+  {
+    c.style.visibility = "visibile";
+  });
+}
