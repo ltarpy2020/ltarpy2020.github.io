0fa01c8b9bdb00a41344d4b900130c8dcc949b7e
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-05 15:37:22 -0400

    Update java.js

--- midterm/scripts/java.js
+++ midterm/scripts/java.js
@@ -1,8 +1,18 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2580
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
-
-\f0\fs24 \cf0 js}
+function funFact()
+{
+  let x = 0;
+  x = Math.floor(Math.random() * 5)
+  switch(x)
+  {
+    case 0:
+      document.getElementById('response').innerHTML = "The Panthers were first founded in 1993";
+    case 1:
+      document.getElementById('response').innerHTML = "The Charlotte Hornets used to be called the Bobcats";
+    case 2:
+      document.getElementById('response').innerHTML = "Chirstian McCaffery was drafted 8th overall back in 2017";
+    case 3:
+      document.getElementById('response').innerHTML = "I was at the playoff game back in 2014 when we beat the AZ Cardinals";
+    case 4:
+      document.getElementById('response').innerHTML = "I love the NFL, but love college basketball more. NC State is my favorite team";
+  }
+}
