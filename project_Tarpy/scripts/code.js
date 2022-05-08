ba0e9c3a74de94a70b21b16dd93bf4444b0b0868
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-08 19:59:51 -0400

    Update code.js

--- project_Tarpy/scripts/code.js
+++ project_Tarpy/scripts/code.js
@@ -1,24 +1,25 @@
-function getData()
+function fn1()
 {
-	var input = document.getElementById('look').value;
-	if (input === "wedding" || "Wedding")
-	{
-	  	document.getElementById('result').innerHTML = "See Figures 1, 2 and 3 in Photo Gallery Below";
-	}
-	if (input === "family" || "Family")
-	{
-		document.getElementById('result').innerHTML = "See Figures 4 and 5 in Photo Gallery Below";
-	}
-	if (input === "gallery" || "Gallery")
-	{
-		document.getElementById('result').innerHTML = "See Figures 6 and 7 in Photo Gallery Below";
-	}
-	if (input === "other" || "Other")
-	{
-		document.getElementById('result').innerHTML = "See Figures 8 in Photo Gallery Below";
-	}
-	else
-	{
-		document.getElementById('result').innerHTML = "Please Enter valid Data";
-	}
+	var inputs = document.getElementById('look').value;
+  
+  if (inputs === "wedding" || inputs === "Wedding")
+  {
+  	document.getElementById('result').innerHTML = "See Figure 1, 2, and 3 on the Photo Gallery Below";
+  }
+  else if (inputs === "family" || inputs === "Family")
+  {
+  	document.getElementById('result').innerHTML = "See Figures 4 and 5 on the Photo Gallery Below";
+  }
+  else if (inputs === "Gallery" || inputs === "gallery")
+  {
+  	document.getElementById('result').innerHTML = "See Figures 6 and 7 on the Photo Gallery Below";
+  }
+  else if (inputs === "Other" || inputs === "other")
+  {
+  	document.getElementById('result').innerHTML = "See Figures 8 on the Photo Gallery Below";
+  }
+  else
+  {
+  	document.getElementById('result').innerHTML = "Please enter valid data";
+  }
 }
