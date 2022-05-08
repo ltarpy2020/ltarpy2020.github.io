efd90c33eaad177cc6a0f4e0421862248301b44b
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-08 19:45:02 -0400

    Update code.js

--- project_Tarpy/scripts/code.js
+++ project_Tarpy/scripts/code.js
@@ -1,8 +1,24 @@
 function getData()
 {
 	var input = document.getElementById('look').value;
-  if (input === "wedding" || "Wedding")
-  {
-  	document.getElementById('result').innerHTML = "See Figures 1, 2 and 3 in Photo Gallery Below";
-  }
+	if (input === "wedding" || "Wedding")
+	{
+	  	document.getElementById('result').innerHTML = "See Figures 1, 2 and 3 in Photo Gallery Below";
+	}
+	if (input === "family" || "Family")
+	{
+		document.getElementById('result').innerHTML = "See Figures 4 and 5 in Photo Gallery Below";
+	}
+	if (input === "gallery" || "Gallery")
+	{
+		document.getElementById('result').innerHTML = "See Figures 6 and 7 in Photo Gallery Below";
+	}
+	if (input === "other" || "Other")
+	{
+		document.getElementById('result').innerHTML = "See Figures 8 in Photo Gallery Below";
+	}
+	else
+	{
+		document.getElementById('result').innerHTML = "Please Enter valid Data";
+	}
 }
