d3dc9520855b76b6534a9638d8fd16a18a940825
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2022-05-09 14:51:42 -0400

    Update code.js

--- project_Tarpy/scripts/code.js
+++ project_Tarpy/scripts/code.js
@@ -23,17 +23,3 @@ function fn1()
   	document.getElementById('result').innerHTML = "Please enter valid data";
   }
 }
-
-function ajaxCall()
-{
-	$.ajax(
-		{
-			"info.json"
-			type: "GET",
-			success: function(data)
-	{
-		var s = JSON.stringify(data);
-		document.getElementById('re').innerHTML = s;
-	}
-};
-}
