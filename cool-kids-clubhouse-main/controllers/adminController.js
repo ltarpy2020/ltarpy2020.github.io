e23d815e4e82a846b758921ed41040f8672648b8
ltarpy2020 <liamtarpy@yahoo.com>
2022-11-02 17:06:40 -0400

    delete1

--- cool-kids-clubhouse-main/controllers/adminController.js
+++
@@ -1,9 +0,0 @@
-//This is where the call back functions for the admin side of the website will go
-exports.index = (req, res)=>{
-    res.render('./events/index');
-};
-
-exports.new = (req, res)=>{
-    res.render('./events/new');
-}
-
--- cool-kids-clubhouse-main/routes/clientRoutes.js
+++
@@ -1,9 +0,0 @@
-//This is where you will handle the routes for clients interactions
-const express = require('express');
-const controller = require('../controllers/clientController');
-
-const router = express.Router();
-
-router.get('/', controller.index);
-
-module.exports = router;
