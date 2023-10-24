23c37fc448db59c61abc4958f4bf4adc359047c4
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2023-10-24 19:14:34 -0400

    Update PokeCopy.py

--- Python/PokeCopy.py
+++ Python/PokeCopy.py
@@ -1,22 +1,39 @@
+
 import random
-z = random.randint(1, 3)
-oppo = random.randint(1, 3)
 
 me = o = 0
 
-if z == 1:
-    print ("This is fire")
-    if oppo == 3:
-        print("you win")
-        me += 1
-        o -= 1
-    elif oppo == 2:
-        print("you lose")
-        me -= 1
-        o += 1
-elif z == 2:
-    print("This is water")
-elif z == 3:
-    print("This is grass")
-    
+for x in range(10):
+    z = random.randint(1, 3)
+    oppo = random.randint(1, 3)
+
+    if z == 1:
+        print ("This is fire")
+        if oppo == 3:
+            me += 1
+            o -= 1
+        elif oppo == 2:
+            me -= 1
+            o += 1
+    elif z == 2:
+        print("This is water")
+        if oppo == 1:
+            me += 1
+            o -= 1
+        elif oppo == 3:
+            me -= 1
+            o += 1
+    elif z == 3:
+        print("This is grass")
+        if oppo == 2:
+            me += 1
+            o -= 1
+        elif oppo == 1:
+            me -= 1
+            o += 1
+
+if me > o:
+    print('You win')
+else:
+    print('You lose')
 print(me, o)
