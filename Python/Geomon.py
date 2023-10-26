f3ce8cb9492568ada72323f4557083ffc29ee35a
Liam Tarpy <32394795+ltarpy2020@users.noreply.github.com>
2023-10-26 17:12:53 -0400

    Update Geomon.py

--- Python/Geomon.py
+++ Python/Geomon.py
@@ -1,4 +1,3 @@
-
 import random
 
 me = o = 0
@@ -7,6 +6,7 @@ element = e = ""
 for x in range(10):
     z = random.randint(1, 12)
     oppo = random.randint(1, 12)
+    jewel = random.randint(1, 100)
 
     if z == 1:
         element = "This is fire"
@@ -16,6 +16,7 @@ for x in range(10):
         if oppo == 12:
             me += 1
             o -= 1
+            print(jewel)
 
     elif z == 2:
         element = "This is water"
@@ -25,6 +26,7 @@ for x in range(10):
         if oppo == 1:
             me += 1
             o -= 1
+            print(jewel)
 
     elif z == 3:
         element = "This is plant"
@@ -34,6 +36,7 @@ for x in range(10):
         if oppo == 2:
             me += 1
             o -= 1
+            print(jewel)
             
     elif z == 4:
         element = "This is electric"
@@ -43,6 +46,7 @@ for x in range(10):
         if oppo == 11:
             me += 1
             o -= 1
+            print(jewel)
         
     elif z == 5:
         element = "This is ground"
@@ -52,6 +56,7 @@ for x in range(10):
         if oppo == 4:
             me += 1
             o -= 1
+            print(jewel)
     
     elif z == 6:
         element = "This is shadow"
@@ -61,6 +66,7 @@ for x in range(10):
         if oppo == 9:
             me += 1
             o -= 1
+            print(jewel)
     
     elif z == 7:
         element = "This is light"
@@ -70,6 +76,7 @@ for x in range(10):
         if oppo == 6:
             me += 1
             o -= 1
+            print(jewel)
     
     elif z == 8:
         element = "This is physical"
@@ -79,6 +86,7 @@ for x in range(10):
         if oppo == 10:
             me += 1
             o -= 1
+            print(jewel)
         
     elif z == 9:
         element = "This is mind"
@@ -88,6 +96,7 @@ for x in range(10):
         if oppo == 6:
             me += 1
             o -= 1
+            print(jewel)
     
     elif z == 10:
         element = "This is ice"
@@ -97,6 +106,7 @@ for x in range(10):
         if oppo == 5:
             me += 1
             o -= 1
+            print(jewel)
         
     elif z == 11:
         element = "This is air"
@@ -106,6 +116,7 @@ for x in range(10):
         if oppo == 3:
             me += 1
             o -= 1
+            print(jewel)
         
     elif z == 12:
         element = "This is toxic"
@@ -115,6 +126,7 @@ for x in range(10):
         if oppo == 7:
             me += 1
             o -= 1
+            print(jewel)
             
     if oppo == 1:
         e = "Opponent has fire"
@@ -146,6 +158,8 @@ for x in range(10):
 
 if me > o:
     print('You win')
+elif me == o:
+    print('You tie')
 else:
     print('You lose')
 print(me, o)
